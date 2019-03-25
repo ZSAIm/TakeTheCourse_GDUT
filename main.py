a8580951a6c630eb5c5d1d29ef126a56083d983d
ZSAIm <34599927+ZSAIm@users.noreply.github.com>
2019-03-26 00:09:49 +0800

    重构代码，加入GUI

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 import threading
@@ -50,53 +49,3 @@ if __name__ == '__main__':
     pool.exit()
     sys.exit()
 
-=======
-import time
-import logging
-import PoolOp
-import cProfile
-import Login
-
-# logging.basicConfig(level=logging.INFO,
-#                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
-
-logging.basicConfig(level=logging.DEBUG,
-                    filename='log/all/log-%s.log' % time.strftime('[%Y-%m-%d]', time.localtime()),
-                    format='%(asctime)s - %(levelname)s: %(message)s'
-                    )
-
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
-console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
-
-def main():
-    logging.info('initialize runing ...')
-    pool = PoolOp.PoolOp()
-    pool.proxy_op.collect()
-    pool.proxy_op.save()
-    # pool.init_proxy()
-    # pool.proxy_op.load()
-    #
-    #
-    logging.info('initialize done.')
-    user_op = Login.UserData()
-    user_op.load()
-    user = user_op.get_userdata()
-    for i in user:
-        pool.build_member(i[0], i[1], i[2])
-
-    pool.batch_run()
-    while True:
-        if pool.isEnd() is True:
-            break
-        time.sleep(1)
-
-    pool.batch_verify()
-
-
-
-if __name__ == '__main__':
-    main()
->>>>>>> origin/master
